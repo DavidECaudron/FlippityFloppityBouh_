e098b08dc75f05e618202a5cfec9ae11f9a8a1e6
Your Name <>
2022-03-02 15:05:12 +0100

    Flippers

---
+++ FlippityFloppityBouh/Assets/Flippity Floppity Bouh/Scripts/Flipper.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    private float _restPosition = 0.0f;
+    private float _pressedPosition = 45.0f;
+    private float _strength = 10000.0f;
+    private float _damper = 150.0f;
+}
