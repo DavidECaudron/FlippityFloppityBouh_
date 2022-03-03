e6507fa54d02dd02f458fdf7a42d96b0910ac177
Your Name <>
2022-03-03 13:35:52 +0100

    InputSystem

---
+++ FlippityFloppityBouh/Assets/Test/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 0.0f;
+
+    private Rigidbody _rigidBody;
+    private float _movementX;
+    private float _movementY;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(_movementX, 0.0f, _movementY);
+        _rigidBody.AddForce(movement * _speed);
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        _movementX = movementVector.x;
+        _movementY = movementVector.y;
+    }
+}
--- FlippityFloppityBouh/Assets/Test/Scripts/Rotation.cs
+++ FlippityFloppityBouh/Assets/Test/Scripts/Rotation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Rotation : MonoBehaviour
 {
     [SerializeField] private GameObject _gameObject;
+    [SerializeField] private float _rotationSpeed;
 
     private void Update()
     {
@@ -13,6 +14,6 @@ public class Rotation : MonoBehaviour
 
     private void RotationGameObject()
     {
-        _gameObject.transform.Rotate(,);
+        _gameObject.transform.Rotate(new Vector3 (0,1,0), 1.0f * _rotationSpeed * Time.deltaTime);
     }
 }
